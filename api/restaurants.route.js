573b570771493855f014384a2257fe128b0296ae
OsbornDelAngel <ozdelangel@gmail.com>
2023-02-01 20:24:58 -0800

    there was a bug on the route page where we couldnt access the apiGetRestaurants method. it is now fixed and now we are able to get a full functioning back end. currently using a VScode extension called thunder client to test all routes. each query parameter is full functional and giving back correct results

--- api/restaurants.route.js
+++ api/restaurants.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 // we are going to make a controller file
 // a controller file is what the route uses
-import RestaurantsCtrl from "./restaurants.controller.js"
+import RestaurantsController from "./restaurants.controller.js"
+
 
 
 const router = express.Router()
 
 
-router.route("/").get(RestaurantsCtrl/apiGetRestaurants)
+router.route("/").get(RestaurantsController.apiGetRestaurants)
 
 export default router
