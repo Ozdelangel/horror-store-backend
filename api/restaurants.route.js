f781de869072d8015cf5ede1285b070813e0c58e
OsbornDelAngel <ozdelangel@gmail.com>
2023-02-05 23:55:51 -0800

    the restaurants router file has beeen updated with two new routes that allow the user to get a list of specific restaurants and all reviews ascociated with that restaurant. Also for front end purposes the router file has also been updated with a new route that returns a list of all cuisines. there will be a dropdown in which users can pick a specific cuisine and get all restaurants associated with that specific cuisine

--- api/restaurants.route.js
+++ api/restaurants.route.js
@@ -11,8 +11,14 @@ const router = express.Router()
 // with this we made so our users can do a simple get request
 // in restaurants.controller is where we make our query strings
 // in the DAO we made what kind of data they can get back
-
+// gets all restaurants
 router.route("/").get(RestaurantsController.apiGetRestaurants)
+// returns a list of restaurants by ID
+// and all reviews
+router.route("/id:id").get(RestaurantsController.apiGetRestaurantById)
+// this will return a list of all cuisines
+// on the front end we want to make a drop down of all cuisines
+router.route("/cuisines").get(RestaurantsController.apiGetRestaurantsByCuisines)
 
 
 // here is where we are going to give them access to be able to do all CRUD actions
